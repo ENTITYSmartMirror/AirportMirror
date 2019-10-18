f1a53b47d11d567932a9060760eefed3f7ecf22f
김찬영 <emrhs1004@naver.com>
2019-10-19 06:04:37 +0900

    picture test

--- config/config.js
+++ config/config.js
@@ -113,7 +113,7 @@ var config = {
 			hide : "true",
 			config: {
 				// See "Configuration options" for more information.
-					url: ["http://s3.us-east-2.amazonaws.com/beautymirror.com/demo/jeju.html"],  // as many URLs you want or you can just ["ENTER IN URL"] if single URL.
+					url: ["http://s3.us-east-2.amazonaws.com/beautymirror.com/demo/jejuteen.html"],  // as many URLs you want or you can just ["ENTER IN URL"] if single URL.
 					updateInterval: 0.5 * 60 * 1000, // rotate URLs every 30 seconds
 					width: "1000", // width of iframe
 					height: "1000", // height of iframe
@@ -121,12 +121,12 @@ var config = {
 			}
 		},
 		{
-			module: "ManCutRegent",
+			module: "ManCutRegent", //9
 			position: "bottom_left",	// This can be any of the regions.
 			hide : "true",
 			config: {
 				// See "Configuration options" for more information.
-					url: ["http://s3.us-east-2.amazonaws.com/beautymirror.com/demo/남자컷리젠트.html"],  // as many URLs you want or you can just ["ENTER IN URL"] if single URL.
+					url: ["https://s3.us-east-2.amazonaws.com/beautymirror.com/demo/jejuadult.html"],  // as many URLs you want or you can just ["ENTER IN URL"] if single URL.
 					updateInterval: 0.5 * 60 * 1000, // rotate URLs every 30 seconds
 					width: "1000", // width of iframe
 					height: "1000", // height of iframe
@@ -134,15 +134,15 @@ var config = {
 			}
 		},
 		{
-			module: "ManCutTwoBlock",
+			module: "ManCutTwoBlock", //10
 			position: "bottom_left",	// This can be any of the regions.
 			hide : "true",
 			config: {
 				// See "Configuration options" for more information.
-					url: ["http://s3.us-east-2.amazonaws.com/beautymirror.com/demo/남자컷투블럭.html"],  // as many URLs you want or you can just ["ENTER IN URL"] if single URL.
+					url: ["http://s3.us-east-2.amazonaws.com/beautymirror.com/demo/jejutest.html"],  // as many URLs you want or you can just ["ENTER IN URL"] if single URL.
 					updateInterval: 0.5 * 60 * 1000, // rotate URLs every 30 seconds
 					width: "1000", // width of iframe
-					height: "1000", // height of iframe
+					height: "1180", // height of iframe
 					frameWidth: "980", // width of embedded iframe, height is beeing calculated by aspect ratio of iframe		
 			}
 		},
@@ -371,7 +371,7 @@ var config = {
 		{ 
 			module: "CategorySelectMain", //24
 			position: "top_left", // This can be any of the regions.
-			header: "메뉴선택", // Optional
+			
 			classes: "default everyone", // Optional
 			config: {
 		
