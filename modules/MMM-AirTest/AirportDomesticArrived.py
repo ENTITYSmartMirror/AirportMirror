b79d6e04566d11e5a1c87b48420e253cce9f23fb
KangMinSoo <kms4381@gmail.com>
2019-10-22 22:17:09 +0900

    Airport International & Domestic Seperate

--- modules/MMM-AirTest/AirportDomesticArrived.py
+++ modules/MMM-AirTest/AirportDomesticArrived.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 now = datetime.now()
 
-url = 'http://openapi.airport.co.kr/service/rest/FlightStatusList/getFlightStatusList?serviceKey=wHP%2BDtLICbhZ5HS1kuRTV4zXVjyuNgmSelChKsogFgLcXenf4DdlUd5lJmR9vnl4ddrBrtFu%2FaFoxhBxJr23Vg%3D%3D&schLineType=I&schIOType=I&schAirCode=GMP&schStTime='
+url = 'http://openapi.airport.co.kr/service/rest/FlightStatusList/getFlightStatusList?serviceKey=wHP%2BDtLICbhZ5HS1kuRTV4zXVjyuNgmSelChKsogFgLcXenf4DdlUd5lJmR9vnl4ddrBrtFu%2FaFoxhBxJr23Vg%3D%3D&schLineType=D&schIOType=I&schAirCode=GMP&schStTime='
 
 b ='&schEdTime=2400'
 d = '&numOfRows=100'
@@ -13,7 +13,7 @@ d = '&numOfRows=100'
 hour = now.hour
 minute = now.minute
 
-'''
+
 hour = "06"
 minute = "00"
 x = url + hour + minute + b + d
@@ -26,8 +26,7 @@ if hour < 10 :
 
 if hour > 9 :
     x = url + str(hour)  + str(minute) + b + d
-
-
+'''
 
 var_url = urlopen(x)
 
@@ -35,13 +34,17 @@ xmldoc = parse(var_url)
 
 for item in xmldoc.iterfind('body/items/item'):
     airFln = item.findtext('airFln')
-    airlineEnglish = item.findtext('airlineEnglish')
-    arrivedEng = item.findtext('arrivedEng')
+    airlineEnglish = item.findtext('airlineKorean')
+    boardingKor = item.findtext('boardingKor')
     std = item.findtext('std')
+    gate = item.findtext('gate')
+    if gate == None :
+        gate = "Delay"
     std1 = int(std)
     std2 = std1%100
     std3 = std1/100
     std4 = int(std3)
-    print(airFln+';'+airlineEnglish+';'+arrivedEng+';'+"{}".format(std4)+": {}".format(std2))
+    if std2 < 10 :
+        std2 = "0" + str(std2)
 
-
+    print(airFln+';'+airlineEnglish+';'+boardingKor+';'+"{}".format(std4)+":{}".format(std2)+';'+str(gate))
