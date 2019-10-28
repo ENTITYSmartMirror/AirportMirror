2a57cf352fde62f5bc47daeb2a1955b652428b94
KangMinSoo <kms4381@gmail.com>
2019-10-28 23:23:53 +0900

    click sound v.2 & FACE API Key update

--- modules/MMM-AirTest/MMM-AirTest.js
+++ modules/MMM-AirTest/MMM-AirTest.js
@@ -28,6 +28,11 @@ Module.register("MMM-AirTest", {
     subelementdiv_p1.className = "p_class1"
     //p1 버튼 클릭시
     subelementdiv_p1.addEventListener("click", () => {
+      var audioFile = new Audio('modules/MMM-AirTest/decision22.mp3');
+
+      audioFile.play();
+    audioFile.currentTime = 0;
+      
       //초기화면은 state=0 이다.
       //초기화면에서 p1버튼(실시간 국내 운항정보) 클릭시
       if(this.config.state==0){
